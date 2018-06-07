dcd7db9d37fc2c8ce6499f8dde44bda6699bc661
Helene <hea5889@thi.de>
2018-06-07 17:44:51 +0200

    LoginServlet
    System.out  usw auskommentiert/gelöscht

--- src/de/login/LoginServlet.java
+++ src/de/login/LoginServlet.java
@@ -20,124 +20,114 @@ import javabeans.LoginBean;
 @WebServlet("/loginservlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	@Resource(lookup="jdbc/MyTHIPool")
+
+	@Resource(lookup = "jdbc/MyTHIPool")
 	private DataSource ds;
-	
-	
-	private LoginBean getLoginData(String usern) throws ServletException{
-		
-		
+
+	private LoginBean getLoginData(String usern) throws ServletException {
+
 		LoginBean benutzer = new LoginBean();
-			
-		try(Connection con = ds.getConnection();
-			//PreparedStatement pstmt = con.prepareStatement("USE thidb;");
-			//ResultSet rsUse = pstmt.executeQuery();
-			PreparedStatement pstmt2 = con.prepareStatement("SELECT * FROM thidb.benutzer WHERE username = ?")){
-				
+
+		try (Connection con = ds.getConnection();
+				// PreparedStatement pstmt = con.prepareStatement("USE thidb;");
+				// ResultSet rsUse = pstmt.executeQuery();
+				PreparedStatement pstmt2 = con.prepareStatement("SELECT * FROM thidb.benutzer WHERE username = ?")) {
+
 			pstmt2.setString(1, usern);
-			try(ResultSet rsSelect = pstmt2.executeQuery();){ //Username wird gesucht
-			
-				if(rsSelect != null && rsSelect.next()) { //Username gefunden
+			try (ResultSet rsSelect = pstmt2.executeQuery();) { // Username wird gesucht
+
+				if (rsSelect != null && rsSelect.next()) { // Username gefunden
 					benutzer.setPasswort(rsSelect.getString("Passwort")); // Passwort des Users in LoginBean einf�gen
 					benutzer.setUsername(usern);
-					benutzer.setBildexist(rsSelect.getBoolean("bildexist")); //true oder false
-					benutzer.setAdminrechte(rsSelect.getBoolean("Adminrechte")); //true oder false
-				
-				}
-				else { //Username nicht gefunden
+					benutzer.setBildexist(rsSelect.getBoolean("bildexist")); // true oder false
+					benutzer.setAdminrechte(rsSelect.getBoolean("Adminrechte")); // true oder false
+
+				} else { // Username nicht gefunden
 					benutzer.setFehlermeldung("⚠ FEHLER:<br> Sie sind nicht registriert!");
 				}
 			}
-						
+
 		} catch (Exception e) {
 			throw new ServletException(e.getMessage());
 		}
-		
-		//System.out.println("user: " + benutzer.getUsername());
-		//System.out.println("passwort: " + benutzer.getPasswort());
+
+		// System.out.println("user: " + benutzer.getUsername());
+		// System.out.println("passwort: " + benutzer.getPasswort());
 		return benutzer;
 	}
-	
-		
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		
-			request.setCharacterEncoding("UTF-8");
-		
-			//Parameter aus dem Formular:
-			String username=request.getParameter("username");
-		    String password=request.getParameter("password");			
-			String loginbutton = request.getParameter("loginbutton");
-			String checkbox = request.getParameter("check");
-			
-			RequestDispatcher disp;
-						
-			if (checkbox == null) { 
-				checkbox ="-"; //sonst NullPointerException Z.102
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+
+		// Parameter aus dem Formular:
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String loginbutton = request.getParameter("loginbutton");
+		String checkbox = request.getParameter("check");
+
+		RequestDispatcher disp;
+
+		if (checkbox == null) {
+			checkbox = "-"; // sonst NullPointerException Z.102
+		}
+
+		switch (loginbutton) {
+
+		case "login": // Buttonvalue
+
+			LoginBean benutzer = getLoginData(username); // Userdaten aus DB holen
+			HttpSession session = request.getSession();
+
+			if (benutzer.getFehlermeldung() != null) { // User nicht gefunden
+				request.setAttribute("lb", benutzer); // request wird nur einmal angezeigt beim Aufruf
+				disp = request.getRequestDispatcher("/home/jsp/login.jsp");
+				disp.forward(request, response);
+
+				// System.out.println("du bist nicht registriert");
 			}
-			//System.out.println(checkbox);
-		    	  
-			switch(loginbutton) {
-			
-				case "login": //Buttonvalue
-					
-					LoginBean benutzer = getLoginData(username); //Userdaten aus DB holen
-					HttpSession session = request.getSession();
-					
-					if(benutzer.getFehlermeldung() != null) { //User nicht gefunden						
-						request.setAttribute("lb", benutzer); //request wird nur einmal angezeigt beim Aufruf
-						disp = request.getRequestDispatcher("/home/jsp/login.jsp");
-						disp.forward(request, response);
-								
-						//System.out.println("du bist nicht registriert");
+
+			else {
+
+				if (!password.equals(benutzer.getPasswort())) { // unterschiedliche Passwoerter
+
+					benutzer.setFehlermeldung("⚠ FEHLER:<br> Das Passwort für den Benutzernamen "
+							+ benutzer.getUsername() + " ist falsch!");
+
+					benutzer.setUsername(null); // Fuer Headeranzeige relevant
+					// System.out.println("falsches passwort");
+					request.setAttribute("lb", benutzer); // request wird nur einmal angezeigt beim Aufruf
+					disp = request.getRequestDispatcher("/home/jsp/login.jsp");
+					disp.forward(request, response);
+
+				}
+
+				else { // Anmeldedaten passen
+
+					session.setAttribute("lb", benutzer);
+
+					if (checkbox.equals("merken")) {
+						Cookie cookie1 = new Cookie("usernameCookie", username);
+						cookie1.setMaxAge(60 * 60 * 24); // Cookie f�r einen Tag
+						cookie1.setPath("/");
+						response.addCookie(cookie1);
+						/*
+						 * Cookie[] cookies = request.getCookies(); if (cookies != null) { for (int i =
+						 * 0; i< cookies.length; i++) { Cookie cookie = cookies[i];
+						 * 
+						 * System.out.println(cookie.getName() + " " + cookie.getValue()); } } else {
+						 * System.out.println("keine Cookies gespeichert"); }
+						 */
 					}
-					
-					else {		
-						
-						if(!password.equals(benutzer.getPasswort())) { //unterschiedliche Passw�rter
-				
-							benutzer.setFehlermeldung("⚠ FEHLER:<br> Das Passwort für den Benutzernamen " + 
-																benutzer.getUsername()+ " ist falsch!");
-							
-							benutzer.setUsername(null); //F�r Headeranzeige relevant
-							//System.out.println("falsches passwort");
-							request.setAttribute("lb", benutzer); //request wird nur einmal angezeigt beim Aufruf
-							disp = request.getRequestDispatcher("/home/jsp/login.jsp");
-							disp.forward(request, response);
-							
-						}
-												
-						else{  //Anmeldedaten passen
-														
-							session.setAttribute("lb", benutzer);
-							
-							if(checkbox.equals("merken")) {
-								Cookie cookie1 = new Cookie("usernameCookie", username);
-								cookie1.setMaxAge(60 * 60 * 24); //Cookie f�r einen Tag
-								cookie1.setPath("/");
-								response.addCookie(cookie1);
-								System.out.println("Cookie eingfügt");
-								
-								Cookie[] cookies = request.getCookies();
-								if (cookies != null) {
-									for (int i = 0; i< cookies.length; i++) {
-										Cookie cookie = cookies[i];
-										
-										System.out.println(cookie.getName() + " " + cookie.getValue());		
-									}
-								}
-								else {
-									System.out.println("keine Cookies gespeichert");
-								}
-							}
-							//history.back()
-							disp = request.getRequestDispatcher("/home/index.jsp");
-							disp.forward(request, response);
-						}
-					}
-					
-					break;
+					// history.back()
+					disp = request.getRequestDispatcher("/home/index.jsp");
+					disp.forward(request, response);
+				}
 			}
+
+			break;
+		}
 	}
 
 }
